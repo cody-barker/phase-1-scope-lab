59bc580e600d067073043e9618cfee50253d0c64
Cody <codybarker.or@gmail.com>
2024-05-24 12:05:19 -0700

    add objects lab solutions

--- index.js
+++ index.js
@@ -18,3 +18,45 @@ function overwriteBestCustomer() {
 function changeLeastFavoriteCustomer() {
   leastFavoriteCustomer = "none";
 }
+
+//objects lab
+
+let employee = {
+  name: "Cody",
+  address: "1630 SE Rural St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+console.log(updateEmployeeWithKeyAndValue(employee, "name", "Mike"));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+console.log(
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "John")
+);
+console.log(employee);
+
+function deleteFromEmployeeByKey(employee, key) {
+  let updatedEmployee = { ...employee };
+  delete updatedEmployee[key];
+  return updatedEmployee;
+}
+
+// console.log(deleteFromEmployeeByKey(employee, "name"));
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+console.log(destructivelyDeleteFromEmployeeByKey(employee, "name"));
+console.log(employee);
